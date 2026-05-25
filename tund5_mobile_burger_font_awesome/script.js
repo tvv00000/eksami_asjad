79dd12204cbc8830db7f6056cbc9bd0fcb8227ef
taavet-valter varkki <tvv@tlu.ee>
2026-05-25 14:19:22 +0300

    tund5

---
+++ tund5_mobile_burger_font_awesome/script.js
@@ -0,0 +1,8 @@
+const mobileMenu = document.querySelector('.mobile-menu')
+const nav = document.querySelector('nav')
+
+mobileMenu.addEventListener('click', () => {
+
+    nav.classList.toggle('active')
+
+})
